b0475ee8399cd7a1612f5346d842cd4521c98731
Nicolaspz <adao.nicolau@weeconnect.co.ao>
2025-08-07 13:40:55 +0100

    remove_next_in_ignore

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a9c6f590b50bdd4f"; }
+/******/ 		__webpack_require__.h = function() { return "bbf23317f2bd6aa0"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
